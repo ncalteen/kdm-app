b3ecfdf7ef3ef4b2ad74c1524452d84d93f21e40
Nick Alteen <ncalteen@github.com>
2026-03-03 15:33:49 -0500

    feat: add WeaponType and Philosophy enums to enhance gameplay mechanics

--- lib/enums.ts
+++ lib/enums.ts
@@ -331,3 +331,101 @@ export enum ShowdownType {
   /** Special Showdown */
   SPECIAL = 'Special'
 }
+
+/**
+ * Weapon Type
+ */
+export enum WeaponType {
+  /** Axe */
+  AXE = 'Axe',
+  /** Bow */
+  BOW = 'Bow',
+  /** Cleaver */
+  CLEAVER = 'Cleaver',
+  /** Club */
+  CLUB = 'Club',
+  /** Dagger */
+  DAGGER = 'Dagger',
+  /** Fan */
+  FAN = 'Fan',
+  /** Fist and Tooth */
+  FIST_AND_TOOTH = 'Fist and Tooth',
+  /** Grand */
+  GRAND = 'Grand',
+  /** Katana */
+  KATANA = 'Katana',
+  /** Katar */
+  KATAR = 'Katar',
+  /** Lantern Armor */
+  LANTERN_ARMOR = 'Lantern Armor',
+  /** Scythe */
+  SCYTHE = 'Scythe',
+  /** Shield */
+  SHIELD = 'Shield',
+  /** Spear */
+  SPEAR = 'Spear',
+  /** Sword */
+  SWORD = 'Sword',
+  /** Sword - Willow */
+  SWORD_WILLOW = 'Sword - Willow',
+  /** Twilight Sword */
+  TWILIGHT_SWORD = 'Twilight Sword',
+  /** Whip */
+  WHIP = 'Whip'
+}
+
+/**
+ * Philosophy
+ */
+export enum Philosophy {
+  /** Ambitionism */
+  AMBITIONISM = 'Ambitionism',
+  /** Champion */
+  CHAMPION = 'Champion',
+  /** Collectivism */
+  COLLECTIVISM = 'Collectivism',
+  /** Deadism */
+  DEADISM = 'Deadism',
+  /** Dreamism */
+  DREAMISM = 'Dreamism',
+  /** Faceism */
+  FACEISM = 'Faceism',
+  /** Gatherism */
+  GATHERISM = 'Gatherism',
+  /** Gourmandism */
+  GOURMANDISM = 'Gourmandism',
+  /** Homicidalism */
+  HOMICIDALISM = 'Homicidalism',
+  /** Impermanism */
+  IMPERMANISM = 'Impermanism',
+  /** Lanternism */
+  LANTERNISM = 'Lanternism',
+  /** Marrowism */
+  MARROWISM = 'Marrowism',
+  /** Monster */
+  MONSTER = 'Monster',
+  /** Optimism */
+  OPTIMISM = 'Optimism',
+  /** Regalism */
+  REGALISM = 'Regalism',
+  /** Romanticism */
+  ROMANTICISM = 'Romanticism',
+  /** Starting */
+  STARTING = 'Starting',
+  /** Survivalism */
+  SURVIVALISM = 'Survivalism',
+  /** Verminism */
+  VERMINISM = 'Verminism',
+  /** Wanderer - Aenas */
+  WANDERER_AENAS = 'Wanderer - Aenas',
+  /** Wanderer - Candy & Cola */
+  WANDERER_CANDY_COLA = 'Wanderer - Candy & Cola',
+  /** Wanderer - Death Drifter */
+  WANDERER_DEATH_DRIFTER = 'Wanderer - Death Drifter',
+  /** Wanderer - Goth */
+  WANDERER_GOTH = 'Wanderer - Goth',
+  /** Wanderer - Lucl */
+  WANDERER_LUCK = 'Wanderer - Luck',
+  /** Wanderer - Preacher */
+  WANDERER_PREACHER = 'Wanderer - Preacher'
+}
