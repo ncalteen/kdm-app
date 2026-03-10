1d5abfcb46016ee329bf03191111da2637be0dab
Nick Alteen <ncalteen@github.com>
2026-03-10 19:51:00 -0400

    fix: add 'use client' directive to list-card and list-item components

--- components/generic/list-card.tsx
+++ components/generic/list-card.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { ListItem, NewListItem } from '@/components/generic/list-item'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
