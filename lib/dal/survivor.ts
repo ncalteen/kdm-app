bf30d59554d47c8411300b86c6c1f96dcc460ad9
Nick Alteen <ncalteen@github.com>
2026-03-11 12:54:59 -0400

    fix: refactor import statements and add getSurvivors and updateSurvivor functions

--- lib/dal/survivor.ts
+++ lib/dal/survivor.ts
@@ -1,5 +1,6 @@
+import { SquiresOfTheCitadelSurvivors } from '@/lib/campaigns/squires'
+import { Tables } from '@/lib/database.types'
 import { createClient } from '@/lib/supabase/client'
-import { SquiresOfTheCitadelSurvivors } from '../campaigns/squires'
 
 /**
  * Add Squires of the Citadel Survivors
@@ -24,4 +25,45 @@ export async function addSquiresOfTheCitadelSurvivors(
 
   if (error)
     throw new Error(`Error Adding Squires to Settlement: ${error.message}`)
+}
+
+/**
+ * Get Survivors for Settlement
+ *
+ * @param settlementId Settlement ID
+ * @returns List of Survivors
+ */
+export async function getSurvivors(
+  settlementId: string
+): Promise<Tables<'survivor'>[]> {
+  const supabase = createClient()
+
+  const { data, error } = await supabase
+    .from('survivor')
+    .select('*')
+    .eq('settlement_id', settlementId)
+    .order('id', { ascending: true })
+
+  if (error) throw new Error(`Error Fetching Survivors: ${error.message}`)
+  return data
+}
+
+/**
+ * Update Survivor
+ *
+ * @param survivorId Survivor ID
+ * @param updates Survivor Updates
+ */
+export async function updateSurvivor(
+  survivorId: string,
+  updates: Partial<Tables<'survivor'>>
+): Promise<void> {
+  const supabase = createClient()
+
+  const { error } = await supabase
+    .from('survivor')
+    .update(updates)
+    .eq('id', survivorId)
+
+  if (error) throw new Error(`Error Updating Survivor: ${error.message}`)
 }
