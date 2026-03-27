46ec5037e8d220583167cbe491cd9e986d196561
Nick Alteen <ncalteen@github.com>
2026-03-27 13:17:44 -0400

    feat: update configuration to allow development origins and additional redirect URLs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  cacheComponents: true
+  cacheComponents: true,
+  allowedDevOrigins: ['192.168.86.21']
 }
 
 export default nextConfig
